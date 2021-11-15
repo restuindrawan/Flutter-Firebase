54df2966743f6eee49653d83a5867649ddd81dcc
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-11-15 11:20:51 +0800

    Declare authController

--- flutter_firebase/lib/app/modules/home/views/home_view.dart
+++ flutter_firebase/lib/app/modules/home/views/home_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/app/controllers/auth_controller.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final authController = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('HomeView'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => authController.signOut(),
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
       body: Center(
         child: Text(

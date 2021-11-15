c99c2a2963f8a775485d8c5369654d8ea09d8c2c
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-11-15 11:21:00 +0800

    Update login_controller.dart

--- flutter_firebase/lib/app/modules/login/controllers/login_controller.dart
+++ flutter_firebase/lib/app/modules/login/controllers/login_controller.dart
@@ -1,4 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController {}
+class LoginController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    emailController.dispose();
+    passController.dispose();
+    super.onClose();
+  }
+}

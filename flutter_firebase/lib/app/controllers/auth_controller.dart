6733ce215ef1174180f32b8bc1c5f2fbacec93c4
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-11-15 11:18:00 +0800

    Add signUp function

--- flutter_firebase/lib/app/controllers/auth_controller.dart
+++ flutter_firebase/lib/app/controllers/auth_controller.dart
@@ -15,6 +15,22 @@ class AuthController extends GetxController {
   Stream<User?> get streamAuthStatus => auth.authStateChanges();
 
   void signUp() {}
+  void signUp(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      Get.offAllNamed(Routes.HOME);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   void signIn(String email, String password) async {
     try {
       await auth.signInWithEmailAndPassword(

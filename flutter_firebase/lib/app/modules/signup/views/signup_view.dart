57b96e4997c6167c2246ad210e2585101d01944a
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-11-15 11:21:17 +0800

    Update signup_view.dart

--- flutter_firebase/lib/app/modules/signup/views/signup_view.dart
+++ flutter_firebase/lib/app/modules/signup/views/signup_view.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/app/controllers/auth_controller.dart';
+import 'package:flutter_firebase/app/routes/app_pages.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/signup_controller.dart';
 
 class SignupView extends GetView<SignupController> {
+  final authController = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SignupView'),
+        title: Text('Daftar'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'SignupView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller.emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: controller.passController,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () => authController.signUp(
+                  controller.emailController.text,
+                  controller.passController.text),
+              child: Text("Daftar"),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Sudah punya akun?"),
+                TextButton(
+                  onPressed: () => Get.back(),
+                  child: Text("Masuk"),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );

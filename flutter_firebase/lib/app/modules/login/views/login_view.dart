8bce67eae77804e7c18c668ab25e6e80a156d651
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-11-15 11:21:09 +0800

    Update login_view.dart

--- flutter_firebase/lib/app/modules/login/views/login_view.dart
+++ flutter_firebase/lib/app/modules/login/views/login_view.dart
@@ -1,42 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/app/controllers/auth_controller.dart';
+import 'package:flutter_firebase/app/routes/app_pages.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  final emailController = TextEditingController();
-  final passController = TextEditingController();
-
   final authController = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller.emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: controller.passController,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () => authController.signIn(
+                  controller.emailController.text,
+                  controller.passController.text),
+              child: Text("Login"),
+            ),
+            ElevatedButton(
+              onPressed: () => authController.signInWithGoogle(),
+              child: Text("Google Sign In"),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Belum punya akun?"),
+                TextButton(
+                  onPressed: () => Get.toNamed(Routes.SIGNUP),
+                  child: Text("Daftar"),
+                ),
+              ],
+            )
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Column(
-            children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: "Email"),
-              ),
-              TextField(
-                controller: passController,
-                decoration: InputDecoration(labelText: "Password"),
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                  onPressed: () => authController.signIn(
-                      emailController.text, passController.text),
-                  child: Text("Login")),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
